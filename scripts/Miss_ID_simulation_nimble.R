f6f25ba8aa99f9f56b528275a414cdc684a970c7
Matthew Farr <farrm@Matthews-MacBook-Pro.local>
2022-02-10 12:41:43 -0800

    Add simulation script using nimble

---
+++ scripts/Miss_ID_simulation_nimble.R
@@ -0,0 +1,309 @@
+library(tidyverse)
+library(nimble)
+
+#Logit function
+logit <- function(pp)
+{
+  log(pp) - log(1-pp)
+}
+
+#Inverse logit function
+expit <- function(eta)
+{
+  1/(1+exp(-eta))
+}
+
+#Generate composition proportions
+comp.fun <- function(nspecies)
+{
+  pi <- runif(nspecies, 0, 1)
+  pi <- pi/sum(pi)
+  return(pi)
+}
+
+#Generate miss ID rates
+missID.fun <- function(nspecies)
+{
+  phi.psi <- matrix(runif(nspecies^2,0,0.25),ncol = nspecies, nrow = nspecies)
+  diag(phi.psi) <- runif(nspecies, 0.8, 1)
+  phi.psi <- phi.psi/apply(phi.psi, MARGIN = 1, sum)
+  return(phi.psi)
+}
+
+#Number of species
+nspecies <- 4
+
+#Number of groups
+ngroups <- 1000
+
+#Community composition
+pi <- comp.fun(nspecies)
+
+#Community epected abundance
+lambda.total <- runif(1, 300, 1000)
+
+#Species expected abundance
+lambda <- lambda.total * pi
+
+#Availability probability
+alpha <- runif(nspecies, 0.5, 1)
+
+#Mean availability probability
+#mu.alpha <- mean(alpha)
+mu.alpha <- sum(pi * alpha)
+
+#Expected community composition (post-contact)
+psi <- alpha * pi / mu.alpha
+
+#Expected miss ID rate
+phi.psi <- missID.fun(nspecies)
+
+#Law of total probability
+phi <- t(phi.psi) %*% psi
+
+#Bayes rule
+# psi.phi <- phi.psi * matrix(rep(psi,nspecies), ncol = nspecies, byrow = T) / matrix(rep(phi,nspecies), ncol = nspecies, byrow = F)
+# I think the above is wrong
+
+
+psi.phi <- matrix(NA, nrow = nspecies, ncol = nspecies)
+for(j in 1:nspecies){
+  for(k in 1:nspecies){
+    psi.phi[j,k] <- psi[j] * phi.psi[j,k] / phi[k]
+  }
+}
+
+#Detection probability
+p <- runif(1, 0, 1)
+
+#Front facing and point of view camera data
+POV <- FF <- matrix(NA, ncol = nspecies, nrow = ngroups)
+
+#Latent miss ID count
+C <- matrix(NA, ncol = nspecies, nrow = ngroups)
+
+#Observer data
+OBS <- array(NA, dim = c(ngroups, nspecies, 2))
+
+for(i in 1:ngroups){
+  
+  FF[i,] <- rpois(nspecies, lambda) 
+  
+  POV[i,] <- rbinom(n = nspecies, size = FF[i,], prob = alpha)
+  
+  tmp <- NULL
+  
+  for(j in 1:nspecies){
+    
+    tmp <- cbind(tmp, rmultinom(1, POV[i,j], phi.psi[j,]))
+    
+  }
+  
+  C[i,] <- apply(tmp, MARGIN = 1, sum)
+  
+  OBS[i,,1] <- rbinom(n = nspecies, size = C[i,], prob = p)
+  
+  OBS[i,,2] <- rbinom(n = nspecies, size = C[i,], prob = p)
+  
+}
+
+code <- nimbleCode({
+  
+  #Priors
+  
+  #Detection probability
+  p ~ dunif(0, 1)
+  
+  #Mean availability probability
+  mu.alpha ~ dunif(0, 1)
+  
+  pi[1:nspecies] ~ ddirch(pi.c[1:nspecies])
+  psi[1:nspecies] ~ ddirch(psi.c[1:nspecies])
+  
+  pi.cap <- sum(pi.star[1:nspecies])
+  psi.cap <- sum(psi.star[1:nspecies])
+  
+  phi.psi[1,1] <- 1-phi.psi[2,1]-phi.psi[3,1]-phi.psi[4,1]
+  phi.psi[2,1] <- exp(m.phi.psi[2,1])/(1+exp(m.phi.psi[2,1]) + exp(m.phi.psi[3,1]) + exp(m.phi.psi[4,1]))
+  phi.psi[3,1] <- exp(m.phi.psi[3,1])/(1+exp(m.phi.psi[2,1]) + exp(m.phi.psi[3,1]) + exp(m.phi.psi[4,1]))
+  phi.psi[4,1] <- exp(m.phi.psi[4,1])/(1+exp(m.phi.psi[2,1]) + exp(m.phi.psi[3,1]) + exp(m.phi.psi[4,1]))
+  phi.psi[1,2] <- exp(m.phi.psi[1,2])/(1+exp(m.phi.psi[1,2]) + exp(m.phi.psi[3,2]) + exp(m.phi.psi[4,2]))
+  phi.psi[2,2] <- 1-phi.psi[1,2]-phi.psi[3,2]-phi.psi[4,2]
+  phi.psi[3,2] <- exp(m.phi.psi[3,2])/(1+exp(m.phi.psi[1,2]) + exp(m.phi.psi[3,2]) + exp(m.phi.psi[4,2]))
+  phi.psi[4,2] <- exp(m.phi.psi[4,2])/(1+exp(m.phi.psi[1,2]) + exp(m.phi.psi[3,2]) + exp(m.phi.psi[4,2]))
+  phi.psi[1,3] <- exp(m.phi.psi[1,3])/(1+exp(m.phi.psi[1,3]) + exp(m.phi.psi[2,3]) + exp(m.phi.psi[4,3]))
+  phi.psi[2,3] <- exp(m.phi.psi[2,3])/(1+exp(m.phi.psi[1,3]) + exp(m.phi.psi[2,3]) + exp(m.phi.psi[4,3]))
+  phi.psi[3,3] <- 1-phi.psi[1,3]-phi.psi[2,3]-phi.psi[4,3]
+  phi.psi[4,3] <- exp(m.phi.psi[4,3])/(1+exp(m.phi.psi[1,3]) + exp(m.phi.psi[2,3]) + exp(m.phi.psi[4,3]))
+  phi.psi[1,4] <- exp(m.phi.psi[1,4])/(1+exp(m.phi.psi[1,4]) + exp(m.phi.psi[2,4]) + exp(m.phi.psi[3,4]))
+  phi.psi[2,4] <- exp(m.phi.psi[2,4])/(1+exp(m.phi.psi[1,4]) + exp(m.phi.psi[2,4]) + exp(m.phi.psi[3,4]))
+  phi.psi[3,4] <- exp(m.phi.psi[3,4])/(1+exp(m.phi.psi[1,4]) + exp(m.phi.psi[2,4]) + exp(m.phi.psi[3,4]))
+  phi.psi[4,4] <- 1-phi.psi[1,4]-phi.psi[2,4]-phi.psi[3,4]
+  m.phi.psi[2,1] ~ dnorm(0,0.01)
+  m.phi.psi[3,1] ~ dnorm(0,0.01)
+  m.phi.psi[4,1] ~ dnorm(0,0.01)
+  m.phi.psi[1,2] ~ dnorm(0,0.01)
+  m.phi.psi[3,2] ~ dnorm(0,0.01)
+  m.phi.psi[4,2] ~ dnorm(0,0.01)
+  m.phi.psi[1,3] ~ dnorm(0,0.01)
+  m.phi.psi[2,3] ~ dnorm(0,0.01)
+  m.phi.psi[4,3] ~ dnorm(0,0.01)
+  m.phi.psi[1,4] ~ dnorm(0,0.01)
+  m.phi.psi[2,4] ~ dnorm(0,0.01)
+  m.phi.psi[3,4] ~ dnorm(0,0.01)
+  
+  # phi.psi[1,1] <- 1 - sum(phi.psi[2:nspecies,1])
+  # phi.psi[2,2] <- 1 - phi.psi[1,2] - sum(phi.psi[3:nspecies,2])
+  # phi.psi[(nspecies-1),(nspecies-1)] <- 1 - sum(phi.psi[1:(nspecies-2),(nspecies-1)]) - phi.psi[nspecies,(nspecies-1)]
+  # phi.psi[nspecies,nspecies] <- 1 - sum(phi.psi[1:(nspecies-1),nspecies])
+  # 
+  # for(j in 2:(nspecies-1)){
+  #   
+  #   phi.psi[j,j] <- 1 - sum(phi.psi[1:(j-1),j]) - sum(phi.psi[(j+1):nspecies,j])
+  #   
+  # }
+  # 
+  # for(k in 1:nspecies){
+  #   
+  #   for(j in (k+1):nspecies){
+  #     
+  #     phi.psi[j,k] <- exp(m.phi.psi[j,k])/(1+exp(m.phi.psi[j,k]) + exp(m.phi.psi[j,k]) + exp(m.phi.psi[j,k]))
+  #     
+  #     m.phi.psi[j,k] ~ dnorm(0, 0.01)
+  #     
+  #   }#end j
+  #   
+  #   
+  #   for(j in 1:(k-1)){
+  #     
+  #     phi.psi[j,k] <- exp(m.phi.psi[j,k])/(1+exp(m.phi.psi[j,k]) + exp(m.phi.psi[j,k]) + exp(m.phi.psi[j,k]))
+  #     
+  #     m.phi.psi[j,k] ~ dnorm(0, 0.01)
+  #     
+  #   }#end j
+  #   
+  # }# end k
+  
+  for(j in 1:nspecies){
+    
+    pi.c[j] <- pi.star[j]/pi.cap
+    psi.c[j] <- psi.star[j]/psi.cap
+    
+    pi.star[j] ~ dgamma(0.01, 0.01)
+    psi.star[j] ~ dgamma(0.01, 0.01)
+    
+  }#end j
+  
+  #Likelihood
+  
+  for(i in 1:ngroups){
+    
+    #Availability of birds post aircraft contact
+    POV.total[i] ~ dbin(mu.alpha, FF.total[i])
+    
+    #Species composition prior to aircraft contact
+    FF[i,1:nspecies] ~ dmulti(pi[1:nspecies], FF.total[i])
+    
+    #Species composition post aircraft contact
+    POV[i,1:nspecies] ~ dmulti(psi[1:nspecies], POV.total[i])
+    
+    #Latent composition of counts corrected for detection
+    C[i,1:nspecies] ~ dmulti(phi[1:nspecies], POV.total[i])
+    
+    for(j in 1:nspecies){
+      
+      for(o in 1:nobs){
+        
+        #Observation process (imperfect detection)
+        OBS[i,j,o] ~ dbin(p, C[i,j])
+        
+      }#end o
+      
+    }#end j
+    
+  }#end i
+  
+  for(j in 1:nspecies){
+    
+    for(k in 1:nspecies){
+      
+      #phi.psi[j,k] <- phi[k] * psi.phi[j,k] / psi[j]
+      psi.phi[j,k] <- psi[k] * phi.psi[j,k] / phi[j]
+      
+      #psi.hold[j,k] <- phi[k] * psi.phi[j,k]
+      phi.hold[j,k] <- psi[k] * phi.psi[j,k]
+      
+    }#end k
+    
+    #Species-specific availability
+    alpha[j] <- psi[j] * mu.alpha / pi[j]
+    
+    #Species proportion post aircraft contact
+    #psi[j] <- sum(psi.hold[j,1:nspecies])
+    phi[j] <- sum(phi.hold[j,1:nspecies])
+    
+  }#end j
+  
+})
+
+#-Compile data-#
+
+data <- list(FF = FF, FF.total = apply(FF, 1, sum),
+             POV = POV, POV.total = apply(POV, 1, sum),
+             OBS = OBS)
+
+constants <- list(nspecies = nspecies, ngroups = ngroups, nobs = 2)
+
+#-Initial values-#
+
+inits <- function(){list(mu.alpha = mean(apply(POV/FF, 2, mean)),
+                         p = p,
+                         pi = apply(FF/apply(FF, 1, sum), 2, mean),
+                         psi = apply(POV/apply(POV, 1, sum), 2, mean))}
+
+#-Parameters to save-#
+
+params <- c("p", "pi", "psi", "phi", "psi.phi", "alpha", "mu.alpha")
+
+#-MCMC settings-#
+
+model <- nimbleModel(code = code,
+                     constants = constants,
+                     data = data,
+                     inits = inits())
+
+MCMCconf <- configureMCMC(model, monitors = params)
+
+MCMCconf$addSampler(target = c("m.phi.psi[2, 1]", "m.phi.psi[3, 1]", "m.phi.psi[4, 1]"),
+                    type = "RW_block")
+
+MCMCconf$addSampler(target = c("m.phi.psi[1, 2]", "m.phi.psi[3, 2]", "m.phi.psi[4, 2]"),
+                    type = "RW_block")
+
+MCMCconf$addSampler(target = c("m.phi.psi[1, 3]", "m.phi.psi[2, 3]", "m.phi.psi[4, 3]"),
+                    type = "RW_block")
+
+MCMCconf$addSampler(target = c("m.phi.psi[1, 4]", "m.phi.psi[2, 4]", "m.phi.psi[3, 4]"), 
+                    type = "RW_block")
+
+
+MCMC <- buildMCMC(MCMCconf)
+
+compiled.model <- compileNimble(model, MCMC)
+
+nc <- 3
+ni <- 20000
+nb <- 10000
+nt <- 1
+
+#-Run model-#
+
+out <- runMCMC(compiled.model$MCMC,
+               niter = ni, nburnin = nb,
+               nchains = nc, thin = nt,
+               samplesAsCodaMCMC = TRUE)
+
+#-Convergence-#
+gelman.diag(out)
+
+plot(out)
