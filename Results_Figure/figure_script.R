92435ea6403ce4417c4e2af67b891cae132fb27f
JamieBrusa <jlbwcc@gmail.com>
2022-10-31 14:03:38 -0600

    Changes for averaging observer counts and estimates

--- Results_Figure/figure_script.R
+++ Results_Figure/figure_script.R
@@ -13,3 +13,14 @@ labs(title="",x ="Species", y = "Count or Abundance", color = "Legend")+
 scale_color_manual(name='',
                   breaks=c('Camera', 'Observer', 'Estimate'),
                   values=c('Estimate'='black', 'Camera'='red', 'Observer'='blue'))
+
+#For averaging observers
+ggplot() + 
+  geom_point(data = results, aes(x = Species, y = FF.Count, color = "Camera"), shape=16, color="red", size=5) +
+  geom_point(data = results, aes(x = Species, y = Obs.Ave.Count, color = "Observer"), color = "blue", size = 3) +
+  geom_pointrange(data = results, aes(x = Species, y = FF.Count, ymin=Obs.Ave.L, ymax=Obs.Ave.U, color = "Estimate"), size = 0.5)+
+  theme(text = element_text(size=20),axis.text.x = element_text(angle=60, hjust=1)) +
+  labs(title="",x ="Species", y = "Count or Abundance", color = "Legend")+
+  scale_color_manual(name='',
+                     breaks=c('Camera', 'Observer', 'Estimate'),
+                     values=c('Estimate'='black', 'Camera'='red', 'Observer'='blue'))
